48a8dc1e7facb874d1753c0894f60498a422a53d
Emran <emran.bm@gmail.com>
2017-01-05 10:25:17 +0330

    initializes offensive agent

--- contest/wildAgent.py
+++ contest/wildAgent.py
@@ -42,8 +42,54 @@ class ReflexCaptureAgent(CaptureAgent):
 class OffensiveReflexAgent(ReflexCaptureAgent):
 
     def getFeatures(self, gameState, action):
-        #TODO
-        return 0
+
+        features = util.Counter()
+        successor = self.getSuccessor(gameState, action)
+
+        features['successorScore'] = self.getScore(successor)
+
+        foodList = self.getFood(successor).asList()
+        if len(foodList) > 0:
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+
+        myPos = successor.getAgentState(self.index).getPosition()
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        inRange = filter(lambda x: not x.isPacman and x.getPosition() != None, enemies)
+        if len(inRange) > 0:
+            positions = [agent.getPosition() for agent in inRange]
+            closest = min(positions, key=lambda x: self.getMazeDistance(myPos, x))
+            closestDist = self.getMazeDistance(myPos, closest)
+            if closestDist <= 5:
+                features['distanceToGhost'] = closestDist
+
+        features['isPacman'] = 1 if successor.getAgentState(self.index).isPacman else 0
+
+        return features
+
+    def getWeights(self, gameState, action):
+        if self.inactiveTime > 80:
+            return {'successorScore': 200, 'distanceToFood': -5, 'distanceToGhost': 2, 'isPacman': 1000}
+        successor = self.getSuccessor(gameState, action)
+        myPos = successor.getAgentState(self.index).getPosition()
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        inRange = filter(lambda x: not x.isPacman and x.getPosition() != None, enemies)
+        if len(inRange) > 0:
+            positions = [agent.getPosition() for agent in inRange]
+            closestPos = min(positions, key=lambda x: self.getMazeDistance(myPos, x))
+            closestDist = self.getMazeDistance(myPos, closestPos)
+            closest_enemies = filter(lambda x: x[0] == closestPos, zip(positions, inRange))
+            for agent in closest_enemies:
+                if agent[1].scaredTimer > 0:
+                    return {'successorScore': 200, 'distanceToFood': -5, 'distanceToGhost': 0, 'isPacman': 0}
+        return {'successorScore': 200, 'distanceToFood': -5, 'distanceToGhost': 2, 'isPacman': 0}
+
+    def __init__(self, index):
+        CaptureAgent.__init__(self, index)
+
+        self.numEnemyFood = "+inf"
+        self.inactiveTime = 0
 
 
 class DefensiveReflexAgent(ReflexCaptureAgent):
